234b58e7b9b504a666186ad446520635336c00d0
fqishuai <rawlinsfeng@163.com>
2022-06-17 22:17:19 +0800

    feat: use @easyops-cn/docusaurus-search-local

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,6 +53,19 @@ const config = {
     ],
   ],
 
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        language: ["en", "zh"],
+      },
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
