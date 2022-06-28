712685853f9706da53fc106a0c67f4b943b6de53
fqishuai <rawlinsfeng@163.com>
2022-06-28 09:39:54 +0800

    feat: fetch调nestjs接口

--- src/pages/todo-list.tsx
+++ src/pages/todo-list.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 
 interface ITodoList {
+  _id: string,
   content: string,
   createTime: string,
   finishTime?: string,
+  name: string,
+  age: number,
 }
 
 export default function TodoList() {
@@ -19,11 +22,68 @@ export default function TodoList() {
     }
   ] as ITodoList[]);
 
+  const handleCreate = async () => {
+    try {
+      let response = await fetch('http://localhost:3001/user/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8' // 注意：不加这个，nestjs的接口@Body拿不到请求数据
+        },
+        body: JSON.stringify({
+          name: 'LeBron',
+          age: 37,
+        })
+      });
+      const res = await response.json();
+      setTodoList(todoList => [...todoList,res.data])
+    } catch (error) {
+      console.log('Request Failed', error);
+    }
+  }
+  const handleUpdate = async () => {
+    try {
+      let response = await fetch('http://localhost:3001/user/update/62b9cebd4af529297c3fc493', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8' // 注意：不加这个，nestjs的接口@Body拿不到请求数据
+        },
+        body: JSON.stringify({
+          name: 'LeBron',
+          age: 16,
+        })
+      });
+      const res = await response.json();
+      // setTodoList(todoList => [...todoList,res.data])
+    } catch (error) {
+      console.log('Request Failed', error);
+    }
+  }
+  const handleDelete = async () => {
+    try {
+      let response = await fetch('http://localhost:3001/user/delete/62b9cebd4af529297c3fc493');
+      const res = await response.json();
+      // setTodoList(todoList => [...todoList,res.data])
+    } catch (error) {
+      console.log('Request Failed', error);
+    }
+  }
+  const handleFindAll = async () => {
+    try {
+      let response = await fetch('http://localhost:3001/user/findAll');
+      const res = await response.json();
+    } catch (error) {
+      console.log('Request Failed', error);
+    }
+  }
+
   return (
     <div className='tailwind'>
-      <button>新建</button>
+      <button onClick={handleCreate}>新建</button>
+      <button onClick={handleUpdate}>更新</button>
+      <button onClick={handleDelete}>删除</button>
+      <button onClick={handleFindAll}>查询</button>
       {
-        todoList.map((item,index) => <div>
+        todoList.map((item,index) => <div key={item._id}>
           <input type="checkbox" />
           <span>{item.content}</span>
           <span className='ml-3'>创建于{item.createTime}</span>
