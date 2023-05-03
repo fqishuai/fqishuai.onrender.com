93f02b41857f35ebdc2e8bc6c466d4543279e75f
fqishuai <rawlinsfeng@163.com>
2023-05-03 22:38:48 +0800

    feat: nodejs notes

--- docs/nodejs/demo/http.js
+++ docs/nodejs/demo/http.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n'); // close the response, adding the content as an argument to end()
+  res.end('Hello World\n'); // close the response
 });
 
 server.listen(port, hostName, () => {
